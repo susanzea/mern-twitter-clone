a753aee69a4fd6390b333000fe747a71f083d00e
Susan Zea <susanzea16@gmail.com>
2022-01-16 20:54:05 -0500

    json web token setup

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,7 @@ const User = require("../../models/User");
 //add bcrypt so we can hash passwords and generate salt
 const bcrypt = require("bcryptjs");
 const keys = require("../../config/keys");
+const jwt = require('jsonwebtoken');
 
 router.get("/test", (req, res) => {
     res.json({ msg: "This is the user route "})
@@ -53,7 +54,24 @@ router.post("/login", (req, res) => {
                 //line above returns a promise that's a boolean which we assign as isMatch below
                 .then(isMatch => {
                     if (isMatch) {
-                        res.json({ msg: "Success!" });
+                        //creating payload of user info to send back
+                        const payload = {
+                            //mongodb id
+                            id: user.id,
+                            handle: user.handle,
+                            email: user.email
+                        }
+                        jwt.sign(
+                            payload, 
+                            keys.secretOrKey, 
+                            {expiresIn: 3600},
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: "Bearer" + token
+                                });
+                            }
+                        )
                     } else {
                         return res.status(400).json({ password: "Incorrect password" });
                     }
